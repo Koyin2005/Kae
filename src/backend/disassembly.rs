328c0db4e6b58e46877f51739f96ec65daa036fd
Koyin2005 <koyinsolaoshodi@hotmail.com>
2024-11-11 00:44:15 +0000

    Changed LoadInt, LoadLocal and LoadGlobal's disassembled format.

--- src/backend/disassembly.rs
+++ src/backend/disassembly.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 use super::instructions::{Chunk, Constant, Instruction};
 
 pub fn disassemble(name:&str,chunk:&Chunk,constants:&[Constant]){
@@ -6,7 +8,9 @@ pub fn disassemble(name:&str,chunk:&Chunk,constants:&[Constant]){
         disassemble_instruction(chunk,ip,instruction,constants);
     }
 }
-
+fn arg_instruction<T:Display>(name:&'static str,arg:T){
+    println!("{} {}",name,arg)
+}
 pub fn disassemble_instruction(chunk:&Chunk,ip:usize,instruction:Instruction,constants:&[Constant]){
     if ip > 0 && chunk.lines[ip] == chunk.lines[ip-1]{
         print!("|  ")
@@ -18,6 +22,15 @@ pub fn disassemble_instruction(chunk:&Chunk,ip:usize,instruction:Instruction,con
     match instruction{
         Instruction::LoadConstant(constant) => {
             println!("LoadConstant {} ({})",constant,&constants[constant as usize])
+        },
+        Instruction::LoadInt(int) => {
+            arg_instruction("LoadInt",int);
+        },
+        Instruction::LoadGlobal(global) => {
+            arg_instruction("LoadGlobal", global);
+        },
+        Instruction::LoadLocal(local) => {
+            arg_instruction("LoadLocal", local);
         },
         _ => {
             
