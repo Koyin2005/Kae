3e0f2f354304b31fe1a540f542ff50db7200a338
Koyin2005 <koyinsolaoshodi@hotmail.com>
2024-11-22 23:31:19 +0000

    Changed irrefutability check for parameter pattern in check_signature method to use is_exhaustive method of PatternChecker instead.

--- src/frontend/typechecking/typechecker.rs
+++ src/frontend/typechecking/typechecker.rs
@@ -150,8 +150,8 @@ impl TypeChecker{
         let params = match function.params.iter().map(|param|{
             let pattern = self.get_pattern(&param.pattern)?;
             let ty = self.check_type(&param.ty)?;
-            if let Err(refutable_pattern) = PatternChecker::check_irrefutable(&pattern){
-                self.error("Can't use non-irrefutable pattern in function parameter.".to_string(), refutable_pattern.location.start_line);
+            if PatternChecker.is_exhaustive(&[&pattern], &ty, &self.type_context){
+                self.error("Can't use non-irrefutable pattern in function parameter.".to_string(), pattern.location.start_line);
                 return Err(TypeCheckFailed);
             }
             if let Err(pattern_type) = PatternChecker::check_pattern_type(&pattern, &ty,&self.type_context){
